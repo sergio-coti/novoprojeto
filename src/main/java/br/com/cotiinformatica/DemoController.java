aaf047763ca75abb88d025294c5f9af512f3f1e4
sergiosistemapeoplenet <sergio@sistemapeoplenet.com.br>
2024-09-14 23:31:37 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/DemoController.java
@@ -0,0 +1,15 @@
+package br.com.cotiinformatica;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class DemoController {
+
+	@GetMapping
+	public String hello() {
+		return "Hello World Novo!";
+	}
+}
